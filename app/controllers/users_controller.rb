d8d55df313a8371285f71c55383c838a5302b406
Phuc dog <onthidaihocabd@gmail.com>
2018-07-18 09:29:19 +0700

    Chapter 8: Basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      log_in @user
       flash[:success] = t("controllers.users_controller.success")
       redirect_to @user
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,12 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true,
     length: {minimum: Settings.password_min_length}
+  def self.digest string
+    cost = if ActiveModel::SecurePassword.min_cost
+             Crypt::Engine::MIN_COST
+           else
+             BCrypt::Engine.cost
+           end
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
